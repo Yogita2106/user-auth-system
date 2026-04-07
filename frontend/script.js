0981486f5bb2a40c1a3e46ff5ef2a4c8e566aebc
Yogita Gupta <yogitagupta21062005@gmail.com>
2026-04-07 12:16:54 +0530

    Live the Backend url

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://user-auth-system-lro0.onrender.com";
 let token = localStorage.getItem("token");
 
 if (token) showDashboard();
